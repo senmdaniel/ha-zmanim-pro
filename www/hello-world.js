32e0cefea8ac35084b479fb47995666c24c14fda
senmdaniel <senmdaniel@hotmail.com>
2026-06-02 12:59:40 +0200

    Add HelloWorldCard custom element implementation

--- www/hello-world.js
+++ www/hello-world.js
@@ -1,1 +1,15 @@
+class HelloWorldCard extends HTMLElement {
+  setConfig(config) {
+    this.innerHTML = `
+      <ha-card header="Hello World">
+        <div style="padding: 16px;">
+          👋 Hello World from HACS!
+        </div>
+      </ha-card>
+    `;
+  }
 
+  set hass(hass) {}
+}
+
+customElements.define("hello-world-card", HelloWorldCard);
