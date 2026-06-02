60b89136bf56a492a3c15489e01989a55d967e99
senmdaniel <senmdaniel@hotmail.com>
2026-06-02 11:50:39 +0200

    Update panel.py

--- custom_components/hello_world/panel.py
+++ custom_components/hello_world/panel.py
@@ -1,19 +1,14 @@
-from homeassistant.components.frontend import add_extra_js_url
+from homeassistant.components import frontend
 
 async def async_setup_panel(hass):
-    hass.http.register_static_path(
-        "/hello-world",
-        hass.config.path("custom_components/hello_world/www"),
-        cache_headers=False,
-    )
-
-    hass.components.frontend.async_register_built_in_panel(
+    frontend.async_register_built_in_panel(
         hass,
         component_name="iframe",
         sidebar_title="Hello World",
         sidebar_icon="mdi:web",
         frontend_url_path="hello-world",
         config={
-            "url": "/hello-world/index.html"
+            "url": "https://senmdaniel.github.io/ha-hello-world-/"
         },
     )
+    
